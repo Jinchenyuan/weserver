dd6287a9931648e62270a184f3e65f6f42f80155
Chenyuan Jin <jinchenyuan@outlook.com>
2025-11-07 22:35:54 +0800

    s3 deploy file

--- docker-build.sh
+++ docker-build.sh
@@ -1,2 +1,4 @@
 docker build --network=host -f Dockerfile.account.api -t account-api:dev .
 docker build --network=host -f Dockerfile.account.service -t account-service:dev .
+docker build --network=host -f Dockerfile.s3.api -t s3-api:dev .
+docker build --network=host -f Dockerfile.s3.service -t s3-service:dev .
--- docker-push.sh
+++ docker-push.sh
@@ -1,5 +1,11 @@
 docker tag account-api:dev 192.168.1.66:7090/lcserver/account-api:dev
 docker tag account-service:dev 192.168.1.66:7090/lcserver/account-service:dev
 
+docker tag s3-api:dev 192.168.1.66:7090/lcserver/s3-api:dev
+docker tag s3-service:dev 192.168.1.66:7090/lcserver/s3-service:dev
+
 docker push 192.168.1.66:7090/lcserver/account-api:dev
 docker push 192.168.1.66:7090/lcserver/account-service:dev
+
+docker push 192.168.1.66:7090/lcserver/s3-api:dev
+docker push 192.168.1.66:7090/lcserver/s3-service:dev
